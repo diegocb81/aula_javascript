53c949deb2a422e2d4246b8329191f7a61183fbb
Diego Barcellos <diegocb81@gmail.com>
2020-09-26 08:52:34 -0300

    Projeto finalizado

--- js/main.js
+++ js/main.js
@@ -1,5 +1,60 @@
 
 
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+    //console.log(document.getElementById("agradecimento"));
+    //alert("Obrigado por clicar");
+}
+
+function redirecionar(){
+    window.open("https://www.waves.com.br/");
+    //window.location.href = "https://www.waves.com.br/";
+}
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    elemento.innerHTML = "Obrigado por passar o mouse";
+    //alert("trocar texto");
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+
+function load(){
+    alert("pagina carregada");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
+
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+*/
+
+/*
+var validar = 0;
+
+function validaIdade(idade){
+    if (idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade");
+validaIdade(idade)
+console.log(validar);
+*/
+
+//alert(soma(5, 10));
+
 /*
 var d = new Date();
 alert(d.getMonth()+1);
